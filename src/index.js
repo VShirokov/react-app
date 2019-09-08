24f038425f0dc17902d9f86e1d3a6979bfe114a3
Широков Владимир <shirr14@gmail.com>
2019-09-09 00:47:20 +0300

    create webpack.config, change project structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './styles/index.less';
+import App from './shared/App/App.jsx';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <Router>
+            <Switch>
+                <Route path="/" name="Home" component={App} />
+                <Route path="/todo" component={App} />
+                {/* <Route path="/icons" component={Icons} />
+                <Route path="/forms" component={Forms} />
+                <Route path="/other" component={Other} />
+                <Route strict path="/dashboard" component={Dashboard} /> */}
+            </Switch>
+        </Router>,
+        document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
